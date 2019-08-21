4df1e2ad3b110727154938e257faee0a956f67e3
Andika Jaya <andika@andika.jaya>
2019-08-21 22:29:08 +0700

    create index, css, js

---
+++ script.js
@@ -0,0 +1,21 @@
+const quotes = [{
+        name: 'Stephen King',
+        quotes: 'Get busy living or get busy dying'
+    },
+    {
+        name: 'John F. Keneddy',
+        quotes: 'Those who dare to fail miserably can achieve greatly'
+    },
+    {
+        name: 'Abraham Lincoln',
+        quotes: 'I am a success today because I had a friend who believed in me and I did not have the heart to let him down.'
+    },
+    {
+        name: 'Leonardo Davinci',
+        quotes: 'It had long since come to my attention that people of accomplishment rarely sat back and let things happen to them. They went out and happened to things'
+    },
+    {
+        name: 'Leo Tolstoy',
+        quotes: 'If you want to be happy, be.'
+    }
+];
